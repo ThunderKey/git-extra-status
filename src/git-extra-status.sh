cbe2648491cdf4a73e3063f2ae09670e9289da74
Nicolas Ganz <nicolas@citrin.ch>
2018-07-11 09:14:03 +0200

    added a "Unknown branch" status if the repo is not connected with a branch

--- src/git-extra-status.sh
+++ src/git-extra-status.sh
@@ -2,6 +2,7 @@
 
 RED='\033[1;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;93m'
 NC='\033[0m'
 
 cwd=$PWD
@@ -16,9 +17,13 @@ for repo in $repos; do
 	repo=$(abspath "$repo")
 	if [[ -d "${repo}/.git" ]]; then
 		cd $repo
-		GIT_STATUS=$(git-status)
-		BARNCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-		printf " [$(basename "$repo")] - ${BARNCH_NAME}\n"
+		BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+		if [[ "$BRANCH_NAME" == "HEAD" ]]; then
+			GIT_STATUS="${YELLOW}?${NC} Unknown branch"
+		else
+			GIT_STATUS=$(git-status)
+		fi
+		printf " [$(basename "$repo")] - ${BRANCH_NAME}\n"
 		if [[ -n $(git status --porcelain) ]]; then
 			printf "    ${GIT_STATUS} ${RED}x${NC} Uncommitted Changes\n"
 		else
